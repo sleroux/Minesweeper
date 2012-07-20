ee1ec62181b2e195a7efd7431ba2653d3496ab9b
Stephan Leroux <stephanleroux@gmail.com>
2012-07-20 00:43:27 -0400

    Moved draw helpers for the cells into private scope

--- scripts/cell.js
+++ scripts/cell.js
@@ -2,48 +2,50 @@ define(['resourceManager'], function (resourceManager) {
     var cell = function (spec) {
         var my = {};
 
-        my.draw = function (context) {
-            if (my.selected) {
-                my.drawSelected(context);
-            } else if (my.flagged) {
-                my.drawFlagged(context);
-            } else if (my.highlighted) {
-                my.drawHighlighted(context);
-            } else {
-                my.drawNormal(context);
-            }
-        };
-
-        my.drawHighlighted = function (context) {
+        // Private Helper Methods
+        function drawHighlighted (context) {
             context.drawImage(spec.highlightedImage, 
                               spec.x, 
                               spec.y, 
                               spec.highlightedImage.width, 
                               spec.highlightedImage.height);
-        };
+        }
 
-        my.drawSelected = function (context) {
+        function drawSelected (context) {
             context.drawImage(my.selectedImage, 
                               spec.x, 
                               spec.y, 
                               my.selectedImage.width, 
                               my.selectedImage.height);
-        };
+        }
 
-        my.drawNormal = function (context) {
+        function drawNormal (context) {
             context.drawImage(resourceManager.images.cell, 
                               spec.x, 
                               spec.y, 
                               resourceManager.images.cell.width, 
                               resourceManager.images.cell.height);
-        };
+        }
 
-        my.drawFlagged = function (context) {
+        function drawFlagged (context) {
             context.drawImage(resourceManager.images.flag, 
                               spec.x, 
                               spec.y, 
                               resourceManager.images.flag.width, 
                               resourceManager.images.flag.height);
+        }
+
+        // Public Methods
+        my.draw = function (context) {
+            if (my.selected) {
+                drawSelected(context);
+            } else if (my.flagged) {
+                drawFlagged(context);
+            } else if (my.highlighted) {
+                drawHighlighted(context);
+            } else {
+                drawNormal(context);
+            }
         };
 
         my.inBounds = function (pos) {
